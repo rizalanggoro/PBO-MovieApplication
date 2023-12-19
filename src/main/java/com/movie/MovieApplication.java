4f2593d8738e692f62f869048c8cff28ef8fc1a0
Rizal Anggoro <gnoogler4@gmail.com>
2023-12-19 23:47:15 +0700

    refactor: change first section to home

--- src/main/java/com/movie/MovieApplication.java
+++ src/main/java/com/movie/MovieApplication.java
@@ -46,7 +46,7 @@ public class MovieApplication extends javax.swing.JFrame {
                 System.out.println("session status: " + sessionResult.getLeft().message);
             }
 
-            navigateToSection(new SectionAuthentication(), true);
+            navigateToSection(new SectionHome(), true);
 
 //            initContent();
 //            navigateToSectionName(SectionMyAccount.name);
